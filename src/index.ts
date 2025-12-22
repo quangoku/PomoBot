c2251141baf75730e6cacb28f5c6754e71544092
quangoku <lequan159753@gmail.com>
2025-12-22 16:16:43 +0700

    add temp server

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { getQuote } from "./bot/command/GetQuote.js";
 import { handleCancelPomo } from "./bot/event/HandleCancelPomo.js";
 import connectDB from "./db/ConnectDB.js";
 import dotenv from "dotenv";
+import express from "express";
 dotenv.config();
 
 async function main() {
@@ -31,3 +32,14 @@ async function main() {
 main()
   .then(() => console.log("Bot is running"))
   .catch(console.error);
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
